b50fbf9d35c30be09077edbda7068036a604488e
张爷 <zhangcandeemail@163.com>
2018-02-06 21:10:44 +0800

    提交配置

--- .build/x86_64-apple-macosx10.10/debug/COpenSSL.build/rc2ofb64.c.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/Tin/Documents/AppDeliverServer/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/rc2ofb64.c \
-  /Users/Tin/Documents/AppDeliverServer/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/include/module.modulemap \
-  /Users/Tin/Documents/AppDeliverServer/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/rc2.h \
-  /Users/Tin/Documents/AppDeliverServer/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/opensslconf.h \
-  /Users/Tin/Documents/AppDeliverServer/.build/checkouts/Perfect-COpenSSL.git--4744624199947727386/COpenSSL/rc2_locl.h
