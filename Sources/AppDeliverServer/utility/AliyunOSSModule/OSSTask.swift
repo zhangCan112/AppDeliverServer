c16f00de78beff4c5a2fd39797d685b18a160ed5
zhangcan <zhangcan@xiaoshouyi.com>
2018-04-20 18:51:58 +0800

    开始使用promise

--- Sources/AppDeliverServer/utility/AliyunOSSModule/OSSTask.swift
+++ Sources/AppDeliverServer/utility/AliyunOSSModule/OSSTask.swift
@@ -10,8 +10,17 @@ import PerfectCURL
 import PerfectCrypto
 import PerfectLib
 import PerfectHTTP
+import PerfectThread
 
 class OSSTask {
+    
+    public struct Error: Swift.Error {
+        /// The curl specific request response code.
+        public let code: Int
+        /// The string message for the curl response code.
+        public let description: String
+    }
+    
     class func start(request: OSSRequest, result: @escaping ((_: Bool, _: String) -> Void)) {
 //        print("GMT时间格式：\(request.GMTDate())\n")
 //        print("签名格式：\(request.signature())\n")
@@ -38,6 +47,30 @@ class OSSTask {
             }
             
         }
+    }
+    
+    class func start(request: OSSRequest) -> Promise<[String:Any]> {
+        
+        let promise = Promise<[String:Any]> { (prom)  in
+            let curlRequest =  request.curlRequest()
+            curlRequest.perform {
+                (confirmation: CURLResponse.Confirmation) in
+                do {
+                    let response = try confirmation()
+                    if response.responseCode == 200 {
+                        prom.set(response.bodyJSON)
+                    } else {
+                        prom.fail(Error(code: response.responseCode, description: response.bodyString))
+                    }
+                } catch let error as CURLResponse.Error {
+                    prom.fail(error)
+                } catch {
+                    prom.fail(Error(code: 400, description: "response解析失败！"))
+                }
+                
+            }
+        }
+        return promise
     }
     
 }
